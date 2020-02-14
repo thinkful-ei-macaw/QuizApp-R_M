e44da935f7ffadaf913be2f07384428c68eb305b
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-14 11:33:11 -0500

    Added questions to store, cleared out index

--- app.js
+++ app.js
@@ -1,29 +1,65 @@
+'use strict';
 /**
  * Example store structure
  */
-const store = {
+const STORE = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What color is broccoli?',
+      question: 'The FDA allows an average of _____ per 100 grams of peanut butter manufactured.',
       answers: [
-        'red',
-        'orange',
-        'pink',
-        'green'
+        'Twelve nail clippings',
+        'Five dead skin fragments',
+        'One rodent hair',
+        'Three insect legs'
       ],
-      correctAnswer: 'green'
+      correctAnswer: 'One rodent hair',
+      source: 'http://www.fda.gov/food/guidanceregulation/guidancedocumentsregulatoryinformation/sanitationtransportation/ucm056174.htm#CHPT3'
     },
     {
-      question: 'What is the current year?',
+      question: 'Jelly beans get their shine from shellac, which is made from _____.',
       answers: [
-        '1970',
-        '2015',
-        '2019',
-        '2005'
+        'Insect poop',
+        'Formaldehyde',
+        'Honey',
+        'Corn syrup'
       ],
-      correctAnswer: '2019'
-    }
+      correctAnswer: 'Insect poop',
+      source: 'http://www.health.com/health/gallery/0,,20588763,00.html#shellac-0'
+    },
+    {
+      question: 'There are over _____ on Mount Everest.',
+      answers: [
+        '1000 indigenous families',
+        '30 species of penguin',
+        '50 different peaks',
+        '200 corpses of climbers and sherpas'
+      ],
+      correctAnswer: '200 corpses of climbers and sherpas',
+      source: 'http://www.bbc.com/future/story/20151008-the-graveyard-in-the-clouds-everests-200-dead-bodies'
+    },
+    {
+      question: 'There is an island near mexico city full of _______.',
+      answers: [
+        'Rabid alligators',
+        'Blue flamingos',
+        'Smelly diapers',
+        'Creepy decaying dolls'
+      ],
+      correctAnswer: 'Creepy decaying dolls',
+      source: 'https://www.atlasobscura.com/places/la-isla-de-las-munecas'
+    },
+    {
+      question: 'You can get _____ from a koala\'s cuddle.',
+      answers: [
+        'First degree burns',
+        'Chlamydia',
+        'A terrible, terrible rash',
+        'Shocked'
+      ],
+      correctAnswer: 'Chlamydia',
+      source: 'http://io9.gizmodo.com/5920738/you-can-accidentally-get-an-std-from-a-koala'
+    },
   ],
   quizStarted: false,
   questionNumber: 0,
